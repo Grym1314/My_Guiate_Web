4c6e8a7e90ad3bdcd51c839cbc0e55e50e34fb65
Grym1314 <chucho_031199@hotmail.com>
2024-03-07 19:02:52 -0600

    Making changes in the banner, no css no issues

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Banner from './view/LandingPage/Banner/index.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Banner/>
     <App />
   </React.StrictMode>,
 )
--- src/view/LandingPage/Banner/index.jsx
+++ src/view/LandingPage/Banner/index.jsx
@@ -2,28 +2,16 @@ import './banner.css';
 
 function Banner() {
     return (
-      <>
-        <div>
-          <a href="https://vitejs.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <h1>Here I will create my first successfull page </h1>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
-      </>
+      <div className='banner-holder'>
+					<div className="content">
+						<header>
+							<h2>The future has landed</h2>
+							<p>And there are no hoverboards or flying cars.<br />
+							Just apps. Lots of mother flipping apps.</p>
+						</header>
+						<span className="image-banner"><img src="images/pic01.jpg" alt="" /></span>
+					</div>
+      </div>
     )
   }
   
