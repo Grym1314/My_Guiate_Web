d7fa8ce320ecaef513fab7763f1f1020f0d88fe0
Grym1314 <chucho_031199@hotmail.com>
2024-03-09 20:58:58 -0600

    the navbar has been edited names and components

---
+++ src/view/LandingPage/Navbar/index.jsx
@@ -0,0 +1,19 @@
+import './navBar.css';
+
+function navBar() {
+    return (
+      <div className='banner-holder'>
+					<div className="content">
+						<div className="header">
+							<h2>Prepara y elige tu propio futuro</h2>
+							<p>Guiate te ofrece la capacidad de prepararte para el examen de admision <br />
+							de cualquier Univerdidad de la Republica mexicana. De manera gratuita y actualizada.</p>
+						</div>
+						<span className="image-banner"><img src="src/assets/Banner/pic01.jpg" alt="" /></span>
+					</div>
+          <a href="#one" className="next-landing scrolly">Next</a>
+      </div>
+    )
+  }
+  
+  export default navBar
