1ac9abdbdcc2d0330b53ea249ae1454dc81ec0ee
Grym1314 <chucho_031199@hotmail.com>
2024-03-17 21:10:14 -0600

    changing tittles and making solutions

--- src/view/LandingPage/Navbar/index.jsx
+++ src/view/LandingPage/Navbar/index.jsx
@@ -3,19 +3,19 @@ import './navBar.css';
 function NavBar() {
     return (
 		<div className="nav-Bar">
-		<h1 className="logo"><a href="index.html">Guiate</a></h1>
+		<h1 className="logo"><a href="main.jsx">Guiate</a></h1>
 		<div className="nav-line">
 			<ul>
-				<li><a href="index.html">Home</a></li>
+				<li><a href="index.html">Inicio</a></li>
 				<li>
-					<a href="#">Layouts</a>
+					<a href="#">Guias</a>
 					<ul>
 						<li><a href="left-sidebar.html">Left Sidebar</a></li>
 						<li><a href="right-sidebar.html">Right Sidebar</a></li>
 						<li><a href="no-sidebar.html">No Sidebar</a></li>
 					</ul>
 				</li>
-				<li><a href="elements.html">Elements</a></li>
+				<li><a href="elements.html">Escuelas</a></li>
 				<li><a href="#" className="button-primary">Sign Up</a></li>
 			</ul>
 		</div>
